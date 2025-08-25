9d5caf51d2ab8a5c9a3deb97fb6767230e450c9c
anita-thilakan <anitathilakan@gmail.com>
2025-08-25 22:39:21 +0530

    CRUD operations for model

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Coding_work\python\python linkedIn Learnings\django_rest_api_work\Ex_Files_RESTful_Web_APIs_Django\Exercise Files\02_02_begin\demo\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
